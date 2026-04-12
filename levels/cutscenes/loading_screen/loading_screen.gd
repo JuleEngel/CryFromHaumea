42c99b054554f5f0d255f16a63b5d16401b5c8b3
Jule Engel <jule.engel1208@gmail.com>
2026-04-12 14:07:14 +0200

    change loading-screen

--- levels/cutscenes/loading_screen/loading_screen.gd
+++ levels/cutscenes/loading_screen/loading_screen.gd
@@ -6,5 +6,5 @@ func _ready() -> void:
 	var tween = create_tween()
 	$Background.modulate = Color.BLACK
 	tween.tween_property($Background, "modulate", Color.WHITE, 0.5) 
-	var timer = get_tree().create_timer(3)
+	var timer = get_tree().create_timer(1.5)
 	timer.timeout.connect(func():get_tree().change_scene_to_packed(next_scene))
