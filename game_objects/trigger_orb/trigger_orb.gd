ef2b3c20bce24cc3fb96037ecb257431f90f0141
LiquidFun <brutenis@gmail.com>
2026-04-12 10:09:45 +0200

    feat: make trigger orbs disappear if one-shot

--- game_objects/trigger_orb/trigger_orb.gd
+++ game_objects/trigger_orb/trigger_orb.gd
@@ -18,6 +18,8 @@ var _time := 0.0
 
 @onready var glow_light: PointLight2D = $GlowLight
 @onready var pickup_sound: AudioStreamPlayer2D = $PickupSound
+@onready var sparkle_particles: CPUParticles2D = $SparkleParticles
+@onready var core_glow: CPUParticles2D = $CoreGlow
 
 func _ready() -> void:
 	body_entered.connect(_on_body_entered)
@@ -35,6 +37,8 @@ func _on_body_entered(body: Node2D) -> void:
 	_triggered = true
 	pickup_sound.play()
 	_execute_action(body)
+	if one_shot:
+		_burst_and_free()
 
 func _execute_action(body: Node2D) -> void:
 	match action:
@@ -43,13 +47,34 @@ func _execute_action(body: Node2D) -> void:
 		Action.INFO_SCREEN:
 			_show_info_screen(body, image)
 
+func _burst_and_free() -> void:
+	glow_light.visible = false
+	core_glow.emitting = false
+	sparkle_particles.one_shot = true
+	sparkle_particles.explosiveness = 1.0
+	sparkle_particles.amount = 40
+	sparkle_particles.initial_velocity_min = 40.0
+	sparkle_particles.initial_velocity_max = 100.0
+	var gradient := Gradient.new()
+	gradient.set_offset(0, 0.0)
+	gradient.set_color(0, sparkle_particles.color)
+	gradient.add_point(0.6, sparkle_particles.color)
+	gradient.set_offset(1, 1.0)
+	gradient.set_color(1, Color(sparkle_particles.color, 0.0))
+	sparkle_particles.color_ramp = gradient
+	sparkle_particles.restart()
+	set_process(false)
+	await get_tree().create_timer(sparkle_particles.lifetime, false).timeout
+	queue_free()
+
+
 func _show_speech_text() -> void:
 	var dialog := DialogTextScene.instantiate()
 	get_tree().current_scene.add_child(dialog)
 	dialog.show_text(text)
 
 func _show_info_screen(body: Node2D, img: Texture2D = null) -> void:
-	await get_tree().create_timer(0.1).timeout
+	await get_tree().create_timer(0.2).timeout
 	var info := InfoScreenScene.instantiate()
 	info.info_text = text
 	if img:
