592d55e30dd6bb605c69a9bf56df5675b6aefa1c
LiquidFun <brutenis@gmail.com>
2026-04-12 14:13:29 +0200

    feat: fix scene change trigger

--- levels/scene_change_trigger.gd
+++ levels/scene_change_trigger.gd
@@ -1,9 +1,13 @@
 extends Area2D
 @export var next_scene:PackedScene
-var loading_screen = preload("res://levels/cutscenes/loading_screen/loading_screen.tscn")
+@export var show_loading_screen := true
+const loading_screen = preload("res://levels/cutscenes/loading_screen/loading_screen.tscn")
 
 func _on_body_entered(body: Node2D) -> void:
-	var current = loading_screen.instantiate()
-	current.next_scene = next_scene
-	get_tree().change_scene_to_node(current)
+	if show_loading_screen:
+		var current = loading_screen.instantiate()
+		current.next_scene = next_scene
+		get_tree().change_scene_to_node(current)
+	else:
+		get_tree().change_scene_to_packed(next_scene)
 	
